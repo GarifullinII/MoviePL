bf6e9f24fd7de68bbaf3c73b6a5ecdeabe5f6d10
GArifullinII <ildaridrisovich@gmail.com>
2022-12-10 22:23:11 +0400

    feat: added symbols

--- MoviePL/MainTabBarViewController.swift
+++ MoviePL/MainTabBarViewController.swift
@@ -12,14 +12,18 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
-        
         let homeVC = UINavigationController(rootViewController: HomeViewController())
         let upcomingVC = UINavigationController(rootViewController: UpcomingViewController())
         let searchVC = UINavigationController(rootViewController: SearchViewController())
         let downloadsVC = UINavigationController(rootViewController: DownloadsViewController())
         
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        upcomingVC.tabBarItem.image = UIImage(systemName: "play.circle")
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadsVC.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
         setViewControllers([homeVC, upcomingVC, searchVC, downloadsVC], animated: true)
+        
     }
 }
 
