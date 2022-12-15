ad6a39f349977542d514dc4ec333fd675a858ae0
GArifullinII <ildaridrisovich@gmail.com>
2022-12-15 23:58:35 +0400

    feat: changed mark in Movie

--- MoviePL/Models/Movie.swift
+++ MoviePL/Models/Movie.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// MARK: - Welcome
+// MARK: - TrendingMoviesResponse
 struct TrendingMoviesResponse: Codable {
     let results: [Result]
 
@@ -42,32 +42,3 @@ enum MediaType: String, Codable {
     case movie = "movie"
     case tv = "tv"
 }
-
-
-
-
-/*
- 
- {
-adult = 0;
-"backdrop_path" = "/198vrF8k7mfQ4FjDJsBmdQcaiyq.jpg";
-"genre_ids" =             (
- 878,
- 28,
- 12
-);
-id = 76600;
-"media_type" = movie;
-"original_language" = en;
-"original_title" = "Avatar: The Way of Water";
-overview = "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.";
-popularity = "1712.743";
-"poster_path" = "/94xxm5701CzOdJdUEdIuwqZaowx.jpg";
-"release_date" = "2022-12-14";
-title = "Avatar: The Way of Water";
-video = 0;
-"vote_average" = "8.295";
-"vote_count" = 149;
-}
- 
- */
