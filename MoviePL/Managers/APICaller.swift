1086f725f05733752686dd9a53282a71f2b657e3
GArifullinII <ildaridrisovich@gmail.com>
2022-12-15 21:00:28 +0400

    feat: created APICaller

---
+++ MoviePL/Managers/APICaller.swift
@@ -0,0 +1,16 @@
+//
+//  APICaller.swift
+//  MoviePL
+//
+//  Created by Ildar Garifullin on 15/12/2022.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "b9f6d050b93761eb17ee176121613b5f"
+}
+
+class APICaller {
+    static let shared = APICaller()
+}
