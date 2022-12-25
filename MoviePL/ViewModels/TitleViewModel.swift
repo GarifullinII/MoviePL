8376d1421f1e7aea1bfbb20946a73a2be0dd1545
GArifullinII <ildaridrisovich@gmail.com>
2022-12-25 20:35:34 +0400

    feat: added TitleTableViewCell

---
+++ MoviePL/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  MoviePL
+//
+//  Created by Ildar Garifullin on 25/12/2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
