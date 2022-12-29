294b68d4c19b6103d37e250e27fef049d47a6c37
GArifullinII <ildaridrisovich@gmail.com>
2022-12-29 21:33:42 +0400

    feat: added new group and changed SearchResultsViewController

--- MoviePL/Controllers/SearchResultsViewController.swift
+++ MoviePL/Controllers/SearchResultsViewController.swift
@@ -11,5 +11,7 @@ class SearchResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemGreen
     }
 }
