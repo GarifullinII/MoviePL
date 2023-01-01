68b30720f52f9e138ffb816d946474771b6612e2
GArifullinII <ildaridrisovich@gmail.com>
2023-01-01 15:43:15 +0400

    feat: changed APICaller, SearchViewController and SearchResultsViewController

--- MoviePL/Controllers/General/SearchResultsViewController.swift
+++ MoviePL/Controllers/General/SearchResultsViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+    public var titles: [Title] = [Title]()
 
-    private let searchResultsCollectionView: UICollectionView = {
+    public let searchResultsCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 100, height: 200)
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10, height: 200)
+        layout.minimumInteritemSpacing = 0
+        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.identifier)
         return collectionView
@@ -20,6 +24,33 @@ class SearchResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchResultsCollectionView)
+        
+        searchResultsCollectionView.delegate = self
+        searchResultsCollectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchResultsCollectionView.frame = view.bounds
+    }
+}
+
+extension SearchResultsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: title.posterPath ?? "")
+        
+        return cell
     }
 }
